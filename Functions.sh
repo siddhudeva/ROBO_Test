f3bbb2d168fd9c511ef8eebc8dd11f8b881cc058
siddhudeva <92065460+siddhudeva@users.noreply.github.com>
2021-12-10 15:40:25 +0530

    function.sh added and it contains sample function

---
+++ Functions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# practice of functions.
+#types of commands in linux are :
+#1, binarys like ls pwd df
+#2, shell builtin commands
+#3, alias - which is to create or customize the command it self. (ex; alias gp='git pull')
+#4, Functions
+
+function1 () {
+echo the value of b=200
+  echo the value of a=${a}
+}
+a=100
+echo value of b is ${b}
