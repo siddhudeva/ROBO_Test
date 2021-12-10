afddfb333cc17056b9c3623f121d2a355241a33d
SiddhuDeva <siddhudangeti@gmail.com>
2021-12-10 22:17:51 +0530

    roboshop project start

--- components/frontend.sh
+++ components/frontend.sh
@@ -2,24 +2,6 @@
 # practice of place
 #  validating root user or not
 
-User=(id -u)
-if [ "$User" -ne 0 ] ; then
-  echo -e "\e[1;31m you are not a Root usr make sure you have a valid permissions\e[0m"
-  exit
-fi
-#validating for component mentioned or not
-
-COMPONENT=$1
-if [ -z "$COMPONENT" ] ; then
-  echo -e "e[1;31m Please specify the COMPONENT name\e[0m"]
-  exit
-fi
-# validating wether the mentioned name is valid or not
-if [ ! -e component/${COMPONENT}.sh ] ; then
-  echo -e "\e[1;31m this file doesn't exists.please check once\e[0m"
-  exit
-fi
-#function is set up for validation of each command
 
 Chec() {
   if [ "$1" -ne 0 ] ; then
