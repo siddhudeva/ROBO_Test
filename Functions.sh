f92be929af2029bebd3b152079e517e0b654833a
siddhudeva <92065460+siddhudeva@users.noreply.github.com>
2021-12-10 16:13:46 +0530

    sample changes

--- Functions.sh
+++ Functions.sh
@@ -5,12 +5,7 @@
 #2, shell builtin commands
 #3, alias - which is to create or customize the command it self. (ex; alias gp='git pull')
 #4, Functions
-Chck() {
-    echo first argument =${1}
-    echo total number of arguments are =${#}
-    echo value of a is =${a}
-    b=400
-  }
-    Chck 340 400 450
-    a=600
-    echo value of b is =${b}
+sample() {
+ echo one
+ echo two
+ }
