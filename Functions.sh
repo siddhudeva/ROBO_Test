1f001eddee5922de451800c46beaabb13c843f14
siddhudeva <92065460+siddhudeva@users.noreply.github.com>
2021-12-10 16:21:24 +0530

    arguments and Functions

--- Functions.sh
+++ Functions.sh
@@ -6,6 +6,11 @@
 #3, alias - which is to create or customize the command it self. (ex; alias gp='git pull')
 #4, Functions
 sample() {
- echo one
- echo two
- }
+  echo First argument = $1
+  echo the totel number of Arguments = ${#}
+  echo a in function = ${a}
+b=200
+}
+a=600
+sample 123 456 567
+echo value of b =${b}
